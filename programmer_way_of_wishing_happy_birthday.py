f6d388e8bd81bf4aa7a0a33bd2559b5689cd0946
Saad Bin Munir <96207544+saadbinmunir@users.noreply.github.com>
2023-05-12 21:48:23 +0100

    Create programmer_way_of_wishing_happy_birthday.py

---
+++ programmer_way_of_wishing_happy_birthday.py
@@ -0,0 +1,29 @@
+import time 
+from random import randint
+for i in range(1,85):
+    print('')
+    space = ''
+    for i in range(1,100):
+        count = randint(1, 30)
+        while(count > 0):
+            space += ' '
+            count -= 1
+        
+        if(i%10==0):
+            print(space +
+                  'Happy Birthday 🎉')
+        elif(i%9 == 0):
+            print(space + "🪅")
+        elif(i%5==0):
+            print(space +"🎈")
+        elif(i%8==0):
+            print(space + "🎁")
+        elif(i%7==0):
+            print(space + "🎂")
+        elif(i%6==0):
+            print(space + "❤️")
+        else:
+            print(space + "🔸")
+        
+        space = ''
+        time.sleep(0.2)
